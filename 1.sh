53c3923f5cdc6825180031955dd0e9822c97e79d
asdsca <a5530960@gmail.com>
2019-11-01 09:48:58 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,6 @@
 yum -y install wget unzip curl
 wget https://github.com/helloxz/ccaa/archive/master.zip
 unzip master.zip && cd ccaa-master && sh ccaa.sh
+chmod +x /etc/rc.d/rc.local
+echo "/usr/sbin/ccaa start" >> /etc/rc.d/rc.local
+wget -O qBittorrentCentOS7install.sh https://zhujiwiki.com/wp-content/uploads/2018/12/qBittorrentCentOS7install.sh && bash qBittorrentCentOS7install.sh
