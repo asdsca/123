9f5daa66d18b6f887c14cedf847a4c29225bcad0
asdsca <a5530960@gmail.com>
2019-11-01 09:49:40 +0800

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate -qO ~/Linux-Remote-Desktop-Environment.sh 'https://raw.githubusercontent.com/iiiiiii1/Linux-Remote-Desktop-Environment/master/Download/Common/Load/Linux-Remote-Desktop-Environment.sh' && chmod a+x ~/Linux-Remote-Desktop-Environment.sh
+bash ~/Linux-Remote-Desktop-Environment.sh -CentOS
