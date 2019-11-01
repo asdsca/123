b2c24c5fb53e4ef687f6f006ecb8320e33863e3c
asdsca <a5530960@gmail.com>
2019-11-01 10:22:03 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,4 @@
+swapoff /var/swapd
 yum -y install wget unzip curl
 wget https://github.com/helloxz/ccaa/archive/master.zip
 unzip master.zip && cd ccaa-master && sh ccaa.sh
@@ -7,6 +8,6 @@ wget -O qBittorrentCentOS7install.sh https://zhujiwiki.com/wp-content/uploads/20
 curl https://rclone.org/install.sh | sudo bash
 wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
 rclone config
-swapoff /var/swapd
+mkdir /data/GoogleDrive
 bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh) | tee v2ray_ins.log
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
