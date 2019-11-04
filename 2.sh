65e9dbf9ba012643f34f7a2fe8c8661ccdf677f5
asdsca <a5530960@gmail.com>
2019-11-05 07:54:17 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,9 +1,11 @@
-yum -y groupinstall "Development Tools"
-yum -y install xz m4 make automake libtool gettext openssl-devel libcurl-devel libevent-devel intltool gtk3-devel psmisc
-wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.94.tar.xz
-tar xf transmission-2.94.tar.xz
-cd transmission-2.94
-./configure
-make
-make install
+yum -y install wget unzip curl
+wget https://github.com/helloxz/ccaa/archive/master.zip
+unzip master.zip && cd ccaa-master && sh ccaa.sh
 cd ~
+chmod +x /etc/rc.d/rc.local
+echo "/usr/sbin/ccaa start" >> /etc/rc.d/rc.local
+curl https://rclone.org/install.sh | sudo bash
+wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
+rclone config
+mkdir /data/GoogleDrive
+bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh) | tee v2ray_ins.log
