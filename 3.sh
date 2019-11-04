980d036b6472693d7b07ba1fe3c0d2911b8b2b66
asdsca <a5530960@gmail.com>
2019-11-04 18:30:11 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,6 +1,6 @@
 dd if=/dev/zero of=/var/swapd bs=1024 count=1048576
 mkswap /var/swapd
-chmod 0777 /var/swapd
+chmod 0644 /var/swapd
 swapon /var/swapd
 echo '/var/swapd   swap   swap   default 0 0' >> /etc/fstab
 echo -e"昨天由于更换控制器导致设备号又乱了。。修改fstab的时候却出现不能修改的问题，显示Read-only file system，搜索了一下找了个临时解决办法搞定了\n"
