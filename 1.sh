7176be583a05cbb976e489e5fcc534a9d1a323f8
asdsca <a5530960@gmail.com>
2019-11-04 21:06:46 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,7 +3,6 @@ wget https://github.com/helloxz/ccaa/archive/master.zip
 unzip master.zip && cd ccaa-master && sh ccaa.sh
 chmod +x /etc/rc.d/rc.local
 echo "/usr/sbin/ccaa start" >> /etc/rc.d/rc.local
-wget -O qBittorrentCentOS7install.sh https://zhujiwiki.com/wp-content/uploads/2018/12/qBittorrentCentOS7install.sh && bash qBittorrentCentOS7install.sh
 curl https://rclone.org/install.sh | sudo bash
 wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
 rclone config
