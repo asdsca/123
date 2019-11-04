50c5dd6ebfcc3ab1871c00760c11a5fa8926546a
asdsca <a5530960@gmail.com>
2019-11-05 07:55:11 +0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,1 +1,10 @@
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+dd if=/dev/zero of=/var/swapd bs=1024 count=1048576
+mkswap /var/swapd
+chmod 0644 /var/swapd
+swapon /var/swapd
+echo '/var/swapd   swap   swap   default 0 0' >> /etc/fstab
+echo -e"昨天由于更换控制器导致设备号又乱了。。修改fstab的时候却出现不能修改的问题，显示Read-only file system，搜索了一下找了个临时解决办法搞定了\n"
+echo -e"1）应急措施：\n"
+echo -e"chmod 666 /dev/null\n"
+echo -e"mount -o remount,rw /\n"
+echo -e"2) 编辑/etc/fstab 然后重启"
