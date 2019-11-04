b69c8b27af25b00fbe9f8e6154788849c272605a
asdsca <a5530960@gmail.com>
2019-11-05 07:54:53 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,10 +1,9 @@
-dd if=/dev/zero of=/var/swapd bs=1024 count=1048576
-mkswap /var/swapd
-chmod 0644 /var/swapd
-swapon /var/swapd
-echo '/var/swapd   swap   swap   default 0 0' >> /etc/fstab
-echo -e"昨天由于更换控制器导致设备号又乱了。。修改fstab的时候却出现不能修改的问题，显示Read-only file system，搜索了一下找了个临时解决办法搞定了\n"
-echo -e"1）应急措施：\n"
-echo -e"chmod 666 /dev/null\n"
-echo -e"mount -o remount,rw /\n"
-echo -e"2) 编辑/etc/fstab 然后重启"
+yum -y groupinstall "Development Tools"
+yum -y install xz m4 make automake libtool gettext openssl-devel libcurl-devel libevent-devel intltool gtk3-devel psmisc
+wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.94.tar.xz
+tar xf transmission-2.94.tar.xz
+cd transmission-2.94
+./configure
+make
+make install
+cd ~
