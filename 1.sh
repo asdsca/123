aa1adfd7fe973c3cdb157fed8c6b8b7836d16c39
asdsca <a5530960@gmail.com>
2019-11-04 18:27:23 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -9,4 +9,3 @@ wget https://www.moerats.com/usr/shell/rclone_debian.sh && bash rclone_debian.sh
 rclone config
 mkdir /data/GoogleDrive
 bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/master/install.sh) | tee v2ray_ins.log
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
