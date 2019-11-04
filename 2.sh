49e5715394b930618315e6b3ef4a500776513d09
asdsca <a5530960@gmail.com>
2019-11-04 19:27:09 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -45,9 +45,6 @@ yum install xrdp -y
 touch ~/.Xclients
 echo "mate-session" > ~/.Xclients
 chmod a+x ~/.Xclients
-touch /home/RdpUser/.Xclients
-echo "mate-session" > /home/RdpUser/.Xclients
-chmod a+x /home/RdpUser/.Xclients
 systemctl enable xrdp
 systemctl start xrdp
 firewall-cmd --add-port=3389/tcp --permanent
@@ -55,24 +52,5 @@ firewall-cmd --reload
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo -e "\033[32m Remote connection RDP is installed  is [OK] \033[0m"
 echo "---------------------------------------------------------------------------------------------------------------------"
-
-echo -e "\n\n\n"
-clear
-echo -e "\n"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "The current default system language is Chinese."
-echo "If you are an English user, please execute ' sudo localectl set-locale LANG=en_US.UTF-8 '"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m Warning: Now, you need to pay attention to the following, please visit this page. \033[0m"
-echo "[Original] One-click installation of Desktop environment, RDP, Windows support for Linux"
-echo "https://tech.cxthhhhh.com/linux/2018/08/07/original-one-click-installation-of-desktop-environment-rdp-windows-support-for-linux-en.html"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "End to Desktop environment and Remote connection tool ! V2.0.2"
-echo -e "\033[33m Everything is ready and the system is restarting. Then you can connect via (RDP)IP:3389. \033[0m"
-echo "Normally, the [root] user is not recommended. The new user [RdpUser] has been created with the password [cxthhhhh.com]. Please change the default password as soon as possible after login."
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from https://tech.cxthhhhh.com - 2018/09/11 - MeowLove"
-echo "---------------------------------------------------------------------------------------------------------------------"
-sleep 5s
 reboot
 echo -e "\n"
