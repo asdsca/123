1e908decfa916a80030c21bf5ab8142e85fcc703
asdsca <a5530960@gmail.com>
2019-11-04 21:04:52 +0800

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,5 +1,9 @@
+yum install -y epel-release
+yum groupinstall "Server with GUI"
+systemctl set-default graphical
 yum install -y tigervnc-server xrdp
 systemctl start xrdp
 systemctl enable xrdp
 firewall-cmd --permanent --add-port=3389/tcp
 firewall-cmd --reload
+reboot
