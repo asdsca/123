95fa5c870928c5e95122d81693c7e5b020a7a7f9
asdsca <a5530960@gmail.com>
2019-11-04 19:31:44 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -32,11 +32,7 @@ echo "--------------------------------------------------------------------------
 
 echo -e "\n"
 sudo cd /root
-rpm -Uvh --force http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm --quiet
 yum install remmina remmina-plugins-* -y
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[32m Remmina FileZilla Firefox Already installed  is [OK] \033[0m"
-echo "---------------------------------------------------------------------------------------------------------------------"
 
 echo -e "\n"
 sudo cd /root
