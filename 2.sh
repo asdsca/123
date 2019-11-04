c66202b839cd5a59f3eee85a4ba18738aebbef13
asdsca <a5530960@gmail.com>
2019-11-04 21:14:21 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,4 +1,3 @@
-yum install screen
-screen -S 1
+screen -S 2
 wget -O qBittorrentCentOS7install.sh https://zhujiwiki.com/wp-content/uploads/2018/12/qBittorrentCentOS7install.sh && bash qBittorrentCentOS7install.sh
 exit
